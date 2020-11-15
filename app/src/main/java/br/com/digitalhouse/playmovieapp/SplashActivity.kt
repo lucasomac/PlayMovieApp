54decb460be01b6e5b4a689689ff2a4598ab5fbd
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-11-14 23:11:21 -0300

    Update README.md

--- app/src/main/java/br/com/digitalhouse/playmovieapp/SplashActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/SplashActivity.kt
@@ -1,12 +1,19 @@
 package br.com.digitalhouse.playmovieapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
