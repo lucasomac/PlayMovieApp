9c461e173f3b78ebe90d8f961275eeaa8d6a3126
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-11-21 21:17:48 -0300

    Criado recycleView dos niveis

---
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/adapters/NivelAdapter.kt
@@ -0,0 +1,54 @@
+package br.com.digitalhouse.playmovieapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.digitalhouse.playmovieapp.R
+import br.com.digitalhouse.playmovieapp.domain.Nivel
+
+class NivelAdapter(
+    private val listaNiveis: ArrayList<Nivel>,
+    val listener: NivelAdapter.NivelListener
+) : RecyclerView.Adapter<NivelAdapter.NivelViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NivelAdapter.NivelViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.card_nivel,
+            parent,
+            false
+        )
+        return NivelViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NivelAdapter.NivelViewHolder, position: Int) {
+        val nivel = listaNiveis.get(position)
+        holder.nivelTextViewNivel.text = "Nídel ${nivel.id}"
+        holder.nivelTextViewContagem.text = "${nivel.progresso}/${nivel.qtdQuestions}"
+        holder.nivelProgressBar.max = nivel.qtdQuestions
+        holder.nivelProgressBar.setProgress(nivel.progresso)
+//        if (listaNiveis.get(position - 1).id != 1) {
+//            if (listaNiveis.get(position - 1).progresso != listaNiveis.get(position - 1).qtdQuestions){
+//
+//            }
+//        }
+    }
+
+    override fun getItemCount(): Int {
+        return listaNiveis.size
+    }
+
+    class NivelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var nivelProgressBar: ProgressBar = itemView.findViewById(R.id.pgProgressNivel)
+        var nivelTextViewContagem: TextView = itemView.findViewById(R.id.tvContagemPerguntas)
+        var nivelTextViewNivel: TextView = itemView.findViewById(R.id.tvNivel)
+    }
+
+    interface NivelListener {
+        fun onClickNivel()
+    }
+}
---
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/domain/Nivel.kt
@@ -0,0 +1,5 @@
+package br.com.digitalhouse.playmovieapp.domain
+
+import java.io.Serializable
+
+data class Nivel(val id: Int, val progresso: Int, val qtdQuestions: Int) : Serializable
