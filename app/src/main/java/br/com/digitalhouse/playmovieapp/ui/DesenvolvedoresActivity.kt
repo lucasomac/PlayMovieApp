05b9c97e01d6186ceda5b652764849c9e6e014e2
carneirosarah <sarahrlcarneiro@gmail.com>
2020-11-22 00:41:42 -0300

    Implementação inicial tela de detalhes

--- app/src/main/java/br/com/digitalhouse/playmovieapp/ui/DesenvolvedoresActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/ui/DesenvolvedoresActivity.kt
@@ -1,5 +1,6 @@
 package br.com.digitalhouse.playmovieapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -62,7 +63,10 @@ class DesenvolvedoresActivity : AppCompatActivity(), DesenvolvedorAdapter.Desenv
     }
 
     override fun onClickDesenvolvedor(item: Int) {
+
         var desenvolvedor = desenvolvedores.get(item)
-        Toast.makeText(this, desenvolvedor.nome, Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, DetalhesDesenvolvedorActivity::class.java)
+        intent.putExtra("desenvolvedor", desenvolvedor)
+        startActivity(intent)
     }
 }
