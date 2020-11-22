78e1fd332d9a5bd6a763e14c497ec504df80c874
carneirosarah <sarahrlcarneiro@gmail.com>
2020-11-21 23:25:44 -0300

    Implementação do layout da tela de desenvolvedores

---
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/adapters/DesenvolvedorAdapter.kt
@@ -0,0 +1,62 @@
+package br.com.digitalhouse.playmovieapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.digitalhouse.playmovieapp.R
+import br.com.digitalhouse.playmovieapp.domain.Desenvolvedor
+
+class DesenvolvedorAdapter(
+    private val listaDesenvolvedores: ArrayList<Desenvolvedor>,
+    val listener: DesenvolvedorListener
+): RecyclerView.Adapter<DesenvolvedorAdapter.DesenvolvedorViewHolder>() {
+
+    interface DesenvolvedorListener {
+        fun onClickDesenvolvedor(item: Int)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DesenvolvedorViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_desenvolvedor,
+            parent,
+            false
+        )
+        return DesenvolvedorViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: DesenvolvedorViewHolder, position: Int) {
+
+        var desenvolvedor = listaDesenvolvedores.get(position)
+        holder.desenvolvedorFoto.setImageResource(desenvolvedor.foto)
+        holder.desenvolvedorNome.text = desenvolvedor.nome
+        holder.desenvolvedorDesc.text = desenvolvedor.descricao
+    }
+
+    override fun getItemCount(): Int = listaDesenvolvedores.size
+
+    inner class DesenvolvedorViewHolder(itemView: View):  RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        var desenvolvedorFoto: ImageView = itemView.findViewById(R.id.desenvolvedorFoto)
+        var desenvolvedorNome: TextView = itemView.findViewById(R.id.desenvolvedorNome)
+        var desenvolvedorDesc: TextView = itemView.findViewById(R.id.desenvolvedorDesc)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onClickDesenvolvedor(position)
+            }
+        }
+    }
+}
--- app/src/main/java/br/com/digitalhouse/playmovieapp/adapters/InteresseAdapter.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/adapters/InteresseAdapter.kt
@@ -5,13 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import br.com.digitalhouse.playmovieapp.R
 import br.com.digitalhouse.playmovieapp.domain.Interesse
 import com.google.android.material.switchmaterial.SwitchMaterial
-import java.security.AccessController.getContext
 
 class InteresseAdapter(
     private val listaInteresses: ArrayList<Interesse>,
