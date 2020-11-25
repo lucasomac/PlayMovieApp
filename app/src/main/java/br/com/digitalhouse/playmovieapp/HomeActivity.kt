adad2e9771c987029f68a05ce4cd784c21633d64
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-11-25 12:23:32 -0300

    Botão de navegação implementado na tela de nivel

--- app/src/main/java/br/com/digitalhouse/playmovieapp/HomeActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/HomeActivity.kt
@@ -2,29 +2,35 @@ package br.com.digitalhouse.playmovieapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import br.com.digitalhouse.playmovieapp.adapters.NivelResumeAdapter
+import br.com.digitalhouse.playmovieapp.domain.Configuracao
 import br.com.digitalhouse.playmovieapp.domain.Nivel
 import br.com.digitalhouse.playmovieapp.domain.Resumo
 import br.com.digitalhouse.playmovieapp.domain.ResumoAdapter
+import br.com.digitalhouse.playmovieapp.ui.view.ConfiguracoesActivity
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.layout_slide_home_niveis.*
 
 class HomeActivity : AppCompatActivity(), NivelResumeAdapter.NivelResumeListener {
-    lateinit var list: ArrayList<Resumo>
+//    lateinit var list: ArrayList<Resumo>
     lateinit var adapter: ResumoAdapter
-    val adapterResume = NivelResumeAdapter(getNiveis(), this)
+
+    //    val adapterResume = NivelResumeAdapter(getNiveis(), this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         btnPlay.setOnClickListener {
             startActivity(Intent(this, NivelActivity::class.java))
         }
+        setSupportActionBar(findViewById(R.id.appBarHome))
+        supportActionBar?.setTitle("")
+        supportActionBar?.setHomeButtonEnabled(true)
 
-        list = getResumos()
-        adapter = ResumoAdapter(this, list)
-        vpHome.adapter = adapter
-        rvNiveisResume.adapter = adapterResume
+//        list = getResumos()
+//        adapter = ResumoAdapter(this, list)
+//        vpHome.adapter = adapter
+//        rvNiveisResume.adapter = adapterResume
     }
 
     fun getResumos(): ArrayList<Resumo> {
@@ -47,5 +53,22 @@ class HomeActivity : AppCompatActivity(), NivelResumeAdapter.NivelResumeListener
 
     override fun onClickNivelResume() {
         TODO("Not yet implemented")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { //Botão adicional na ToolBar
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                startActivity(
+                    Intent(
+                        this,
+                        ConfiguracoesActivity::class.java
+                    )
+                ) //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity() //Método para matar a activity e não deixa-lá indexada na pilhagem
+            }
+            else -> {
+            }
+        }
+        return true
     }
 }
