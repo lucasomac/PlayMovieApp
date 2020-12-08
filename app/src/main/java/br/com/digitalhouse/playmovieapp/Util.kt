9c9cdad5609347a1c09234d0c5e4614180317989
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-12-08 16:18:09 -0300

    [HOME] Cards da home agora com fragments e evento de onclick

--- app/src/main/java/br/com/digitalhouse/playmovieapp/Util.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/Util.kt
@@ -1,6 +1,19 @@
 package br.com.digitalhouse.playmovieapp
 
+import br.com.digitalhouse.playmovieapp.domain.Nivel
+
 val API_KEY = System.getenv("apikey")
 val API_MARVEL_URL = "https://gateway.marvel.com/v1/public/"
 val LANGUAGE = "pt-BR"
 val INCLUDE_ADULT = true
+
+fun getNiveis(): ArrayList<Nivel> {
+    return arrayListOf<Nivel>(
+        Nivel(1, 12, 15),
+        Nivel(2, 11, 15),
+        Nivel(3, 8, 15),
+        Nivel(4, 15, 15),
+        Nivel(5, 1, 15),
+        Nivel(6, 5, 15)
+    )
+}
