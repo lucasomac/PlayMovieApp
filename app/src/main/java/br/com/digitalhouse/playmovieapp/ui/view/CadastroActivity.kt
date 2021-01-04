99743fd64d98267e3d4879087719bc5deefb0e9c
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-01-04 20:56:15 -0300

    Splash + Login + Cadastro

--- app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/CadastroActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/CadastroActivity.kt
@@ -1,18 +1,24 @@
 package br.com.digitalhouse.playmovieapp.ui.view
 
 import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import br.com.digitalhouse.playmovieapp.R
+import kotlinx.android.synthetic.main.activity_cadastro.*
 import kotlinx.android.synthetic.main.app_toolbar.*
+
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
         setSupportActionBar(findViewById(R.id.material_toolbar))
-//        supportActionBar?.setTitle("")
-//        supportActionBar?.setHomeButtonEnabled(true)
         initToolbar()
+
+        init()
     }
 
     private fun initToolbar() {
@@ -21,5 +27,10 @@ class CadastroActivity : AppCompatActivity() {
         supportActionBar?.setTitle("Criar nova conta")
         supportActionBar?.setDisplayHomeAsUpEnabled(true); //Mostrar o botão
         supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    private fun init() {
+        val htmlAsString = getString(R.string.termos_html)
+        textView_termos.text = HtmlCompat.fromHtml(htmlAsString, HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
