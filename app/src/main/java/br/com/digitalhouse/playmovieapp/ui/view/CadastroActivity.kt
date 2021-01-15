ba8a88b0f4a06ab821b64c2c81e1ee97eb12a1cf
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-01-14 22:57:30 -0300

    Login com email e senha funcional

--- app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/CadastroActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/CadastroActivity.kt
@@ -1,10 +1,14 @@
 package br.com.digitalhouse.playmovieapp.ui.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import br.com.digitalhouse.playmovieapp.R
 import br.com.digitalhouse.playmovieapp.databinding.ActivityCadastroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class CadastroActivity : AppCompatActivity() {
@@ -15,8 +19,10 @@ class CadastroActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(findViewById(R.id.material_toolbar))
         initToolbar()
-
         init()
+        binding.buttonCriarConta.setOnClickListener {
+            getDataFields()
+        }
     }
 
     private fun initToolbar() {
@@ -31,5 +37,46 @@ class CadastroActivity : AppCompatActivity() {
         val htmlAsString = getString(R.string.termos_html)
         binding.textViewTermos.text =
             HtmlCompat.fromHtml(htmlAsString, HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+
+    //Captura os dado sidgitados nos campos
+    fun getDataFields() {
+        val email = binding.textEmail.text.toString()
+        val password = binding.textPassword.text.toString()
+        val passwordConfirm = binding.textPasswordConfirm.text.toString()
+        val aptEmail = email.isNotEmpty()
+        val aptPassword = password.isNotEmpty()
+//        val aptPasswordConfirm = passwordConfirm.isNotEmpty()
+        if (aptEmail && aptPassword && password.equals(passwordConfirm)) {
+            sendDataFirebase(email, password)
+        } else {
+            showMsg("Preencha todos os dados")
+        }
+    }
+
+    fun sendDataFirebase(email: String, password: String) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val firebaseUser: FirebaseUser = task.result?.user!!
+                    val key = firebaseUser.uid
+                    val email = firebaseUser.email.toString()
+                    showMsg("Cadastro realizado com sucesso!")
+                    callMain(key, email)
+                } else {
+                    showMsg(task.exception.toString())
+                }
+            }
+    }
+
+    fun showMsg(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun callMain(key: String, email: String) {
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.putExtra("key", key)
+        intent.putExtra("email", email)
+        startActivity(intent)
     }
 }
--- app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/LoginActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/LoginActivity.kt
@@ -2,29 +2,108 @@ package br.com.digitalhouse.playmovieapp.ui.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import br.com.digitalhouse.playmovieapp.R
 import br.com.digitalhouse.playmovieapp.databinding.ActivityLoginBinding
+import br.com.digitalhouse.playmovieapp.ui.viewModel.LoginViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityLoginBinding
-
+    private lateinit var googleSignInClient: GoogleSignInOptions
+    private lateinit var auth: FirebaseAuth
+    val viewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+        googleSignInClient = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
 
-        setListeners()
+//        mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this)
+//            .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInClient)
+//            .build();
+        binding.buttonLogin.setOnClickListener(this)
+        binding.textViewCriarConta.setOnClickListener(this)
+        binding.includeSocialNetworksLogin.imageViewGoogle.setOnClickListener(this)
     }
 
-    private fun setListeners() {
-        binding.buttonLogin.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
-        }
+//    private fun signInGoogle() {
+//        Intent signInIntent = Auth . GoogleSignInApi . getSignInIntent (mGoogleApiClient);
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+//    }
 
-        binding.textViewCriarConta.setOnClickListener {
-            startActivity(Intent(this, CadastroActivity::class.java))
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun callRegister() {
+        val intent = Intent(this, CadastroActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun getDataFieldsLogin() {
+        val email = binding.textEmail.toString()
+        val password = binding.textPassword.text.toString()
+        val aptEmail = email.isNotEmpty()
+        val aptPassword = password.isNotEmpty()
+        if (aptEmail && aptPassword) {
+            sendDataFirebase(email, password)
+        } else {
+            showMsg("Preencha todos os dados")
         }
     }
+
+    fun sendDataFirebase(email: String, password: String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val firebaseUser: FirebaseUser = task.result?.user!!
+                    val key = firebaseUser.uid
+                    val email = firebaseUser.email
+                    showMsg("Login realizado com sucesso!")
+                    callMain(key, email.toString())
+                } else {
+                    showMsg(task.exception.toString())
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            callMain(user.uid, user.email.toString())
+        }
+    }
+
+
+    fun showMsg(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun callMain(key: String, email: String) {
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.putExtra("key", key)
+        intent.putExtra("email", email)
+        startActivity(intent)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.button_login -> getDataFieldsLogin()
+//            R.id.imageViewGoogle -> signInGoogle()
+            R.id.textView_criar_conta -> callRegister()
+        }
+    }
+
 }
