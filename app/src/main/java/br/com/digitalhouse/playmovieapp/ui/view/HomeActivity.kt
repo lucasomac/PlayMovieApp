f3bbfa81c52b929b0894e92804a697c02d6e3668
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-01-17 22:28:53 -0300

    Login com google finalizado.

--- app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/HomeActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/HomeActivity.kt
@@ -2,35 +2,50 @@ package br.com.digitalhouse.playmovieapp.ui.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import br.com.digitalhouse.playmovieapp.R
 import br.com.digitalhouse.playmovieapp.databinding.ActivityHomeBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnPlay.setOnClickListener {
-            startActivity(Intent(this, NivelActivity::class.java))
-        }
+        binding.btnPlay.setOnClickListener(this)
 
-        binding.ivSugestao.setOnClickListener {
-            startActivity(Intent(this, SugestaoActivity::class.java))
-        }
+        binding.ivSugestao.setOnClickListener(this)
 
-        binding.ivLupa.setOnClickListener {
-            startActivity(Intent(this, PesquisaActivity::class.java))
-        }
+        binding.ivLupa.setOnClickListener(this)
 
-        binding.btnConfigs.setOnClickListener {
-            startActivity(Intent(this, ConfiguracoesActivity::class.java))
-        }
+        binding.btnConfigs.setOnClickListener(this)
 
-        binding.btnAjustes.setOnClickListener {
-            startActivity(Intent(this, InteressesActivity::class.java))
+        binding.btnAjustes.setOnClickListener(this)
+    }
+
+    fun callLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        logOut()
+        finish()
+        startActivity(intent)
+    }
+
+    fun logOut() {
+        Firebase.auth.signOut()
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.btnPlay -> startActivity(Intent(this, NivelActivity::class.java))
+            R.id.ivSugestao -> startActivity(Intent(this, SugestaoActivity::class.java))
+            R.id.ivLupa -> startActivity(Intent(this, PesquisaActivity::class.java))
+            R.id.btnConfigs -> startActivity(Intent(this, ConfiguracoesActivity::class.java))
+            R.id.btnAjustes -> startActivity(Intent(this, InteressesActivity::class.java))
         }
     }
 }
