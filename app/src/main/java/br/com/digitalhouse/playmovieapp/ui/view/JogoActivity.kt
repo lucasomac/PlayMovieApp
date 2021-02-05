b470c480833e7ac8f650efa8546dca92bfd3549a
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-02-04 23:22:33 -0300

    Tela de jogo finalizada! Faltando dialog do parabéns.

--- app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/JogoActivity.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/ui/view/JogoActivity.kt
@@ -1,9 +1,14 @@
 package br.com.digitalhouse.playmovieapp.ui.view
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.window.Dialog
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import br.com.digitalhouse.playmovieapp.BASE_URL_IMAGE
 import br.com.digitalhouse.playmovieapp.R
@@ -14,35 +19,81 @@ import com.bumptech.glide.Glide
 
 class JogoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityJogoBinding
-    private lateinit var image: String
     private lateinit var totalQuestions: String
     private lateinit var id: String
+    private var pontuacao = 100
+    private var tentativas = 0
     private val viewModdel: JogoViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityJogoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val prefs = getSharedPreferences(R.string.prefs_file.toString(), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null).toString()
         val bundle = intent.extras
-        image = bundle?.get("image").toString()
         totalQuestions = bundle?.get("totalQuestions").toString()
         id = bundle?.get("id").toString()
-        initToolbar()
+        val numero = bundle?.get("number")
+        initToolbar(numero.toString().toInt() + 1)
         viewModdel.question.observeForever {
             Glide.with(this).asBitmap()
                 .load("$BASE_URL_IMAGE/original/${it.image}")
                 .placeholder(R.drawable.progress_animation).into(binding.capaImage)
+            var lista = arrayListOf(it.optionA,
+                it.optionC,
+                it.optionB,
+                it.title)
+            lista.sort()
+            binding.btn01.text = lista[0]
+            binding.btn02.text = lista[1]
+            binding.btn03.text = lista[2]
+            binding.btn04.text = lista[3]
         }
-        viewModdel.getQuestion(image)
-        binding.btnResposta.setOnClickListener {
-            if (binding.tvResposta.text.toString().equals(viewModdel.question.value?.title)) {
-
-            }
+        viewModdel.getQuestion(id)
+        binding.btn01.setOnClickListener {
+            compareButton(it as Button, email)
+        }
+        binding.btn02.setOnClickListener {
+            compareButton(it as Button, email)
+        }
+        binding.btn03.setOnClickListener {
+            compareButton(it as Button, email)
+        }
+        binding.btn04.setOnClickListener {
+            compareButton(it as Button, email)
+        }
+        binding.btnDica.setOnClickListener {
+            tentativas++
         }
     }
 
-    private fun initToolbar() {
+    fun compareButton(button: Button, email: String) {
+        if (button.text.toString().equals(viewModdel.question.value?.title)) {
+            calculaPontos()
+            Log.i("ANSW", "" + pontuacao + " " + tentativas)
+            viewModdel.insereRespondida(viewModdel.question.value?.id.toString(),
+                email,
+                pontuacao,
+                tentativas)
+            Toast.makeText(this, "Aceertouuuuuuu!!", Toast.LENGTH_LONG).show()
+            onBackPressed()
+        } else {
+            tentativas++
+            button.isEnabled = false
+            button.isClickable = false
+            Toast.makeText(this, "Errrrouuuu!!", Toast.LENGTH_LONG).show()
+            button.setTextColor(ContextCompat.getColor(button.context, R.color.white))
+            button.setBackgroundColor(ContextCompat.getColor(button.context, R.color.gray_acent))
+        }
+    }
+
+    fun calculaPontos() {
+        pontuacao = if (pontuacao > 0) 100 - (tentativas * 30) else 0
+    }
+
+    private fun initToolbar(numero: Int) {
         setSupportActionBar(binding.includeConfigToolbar.materialToolbar)
-        setTitle("Pergunta ${id} de ${totalQuestions}")
+        setTitle("Pergunta ${numero} de ${totalQuestions}")
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
--- app/src/main/java/br/com/digitalhouse/playmovieapp/ui/viewModel/JogoViewModel.kt
+++ app/src/main/java/br/com/digitalhouse/playmovieapp/ui/viewModel/JogoViewModel.kt
@@ -5,34 +5,42 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.digitalhouse.playmovieapp.domain.nivel.Question
+import br.com.digitalhouse.playmovieapp.domain.nivel.QuestionAnswered
 import br.com.digitalhouse.playmovieapp.services.collectionQuestions
+import br.com.digitalhouse.playmovieapp.services.collectionUsers
 import com.google.firebase.firestore.ktx.getField
 import kotlinx.coroutines.launch
 
 class JogoViewModel : ViewModel() {
     var question = MutableLiveData<Question>()
-    fun getQuestion(image: String) {
+
+    fun getQuestion(id: String) {
         viewModelScope.launch {
-            collectionQuestions.whereEqualTo("image", image)
+            collectionQuestions.document(id)
                 .get()
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        val listQuestions = arrayListOf<Question>()
-                        for (document in task.result!!) {
-                            val question = Question(
-                                document.id as String,
-                                document.data["level"] as Number,
-                                document.data["title"] as String,
-                                document.data["image"] as String,
-                                document.data["overview"] as String,
-                            )
-                            listQuestions.add(question)
-                        }
-                        question.value = listQuestions[0]
+                        Log.i("IDd", task.result?.data.toString())
+                        val questionReturn = Question(
+                            task.result?.id as String,
+                            task.result?.data!!["level"] as Number,
+                            task.result?.data!!["title"] as String,
+                            task.result?.data!!["image"] as String,
+                            task.result?.data!!["overview"] as String,
+                            task.result?.data!!["optionA"] as String,
+                            task.result?.data!!["optionB"] as String,
+                            task.result?.data!!["optionC"] as String
+                        )
+                        question.value = questionReturn
                     } else {
                         Log.i("SubNivelViewModel", "ERROR.", task.exception)
                     }
                 }
         }
+    }
+
+    fun insereRespondida(id: String, email: String, pontuacao: Int, tentativas: Int) {
+        val answered = QuestionAnswered(tentativas, pontuacao)
+        collectionUsers.document(email).collection("answered").document(id).set(answered)
     }
 }
